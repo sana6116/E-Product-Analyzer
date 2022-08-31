02cf604b24e5989a9bf92eb9e7a3528a58163532
sana6116 <110883960+sana6116@users.noreply.github.com>
2022-08-31 10:51:38 +0530

    final draft

---
+++ README.md
@@ -0,0 +1,27 @@
+# Scraping of Zomato data
+
+### What is it?
+It is desgined to scape data from E-commerse-wesite about any product
+
+### Main Features:
+
+1. the product is specified by the user 
+
+2. It scrapes every products's mentioned by the user  **Name**, **Price**  .
+
+3. the scraped data is saved to  csv file 
+
+### Dependencies
+
+1. **selenium** : Selenium is one of the most renowned open-source test automation frameworks. Selenium allows test automation of web-apps or websites across different browsers & operating systems.
+
+2. **Selenium/Webdriver** : Selenium WebDriver allows you to directly interact with the browsers through your automation test scripts.
+
+3. **Time** : The Python time module provides many ways of representing time in code, such as objects, numbers, and strings. It also provides functionality other than representing time, like waiting during code execution and measuring the efficiency of your code.
+
+4. **Pandas** : Pandas is an open source Python package that is most widely used for data science/data analysis and machine learning tasks
+
+5. **Itertools** :Itertools is a module in Python, it is used to iterate over data structures that can be stepped over using a for-loop
+
+### Getting help
+for questions use **Stack OverFlow** or **geeksforgeeks**
--- main.py
+++ main.py
@@ -1,47 +1,70 @@
+import csv
+from re import template
+from webbrowser import Chrome
+from bs4 import BeautifulSoup
 from selenium import webdriver
-from selenium.webdriver.common.by import By
-from webdriver_manager.chrome import ChromeDriverManager
-from selenium.webdriver.common.keys import Keys
-import itertools
 import pandas as pd
-import time
 
-searching_data = input("what you wanna search: ")
-driver = webdriver.Chrome(ChromeDriverManager().install())
-driver.get("https://www.amazon.com/")
-searchs = driver.find_element(
-    By.XPATH, '//*[@id="twotabsearchtextbox"]'
-)
+def get_url(searching_item):
+    site = 'https://www.amazon.com/s?k={}&ref=nb_sb_noss_1'
 
-searchs.send_keys(searching_data)
-searchs.send_keys(Keys.ENTER)
+    searching_item = searching_item.replace(" ","+")
+    url = site.format(searching_item)
 
-time.sleep(10)
+    url += '&page{}'
 
-new_list = []
+    return site.format(searching_item)
 
-product = driver.find_elements(
-    By.CLASS_NAME, "a-size-medium"
-)
+def detail(product):
+    a_tag = product.h2.a
+    description = a_tag.text.strip()
+    new_url = 'https://www.amazon.com'+a_tag.get('href')
 
-product_list = [item.text for item in product]
+    try:
+        parent_tag = product.find('span', 'a-price')
+        price = parent_tag.find('span','a-offscreen').text
 
-def details(item):
-    for item in product_list:
-        names = driver.find_element(
-            By.CLASS_NAME, 'a-size-medium'
-        )
-        prices = driver.find_element(
-            By.CLASS_NAME, 'a-price'
-        )
-    new_list.append({
-        "Name" :names.text,
-        "Price" :prices.text
-    })
-    return new_list
-final_list = list(itertools.chain(*[details(item)for item in product_list]))
-df = pd.DataFrame(final_list)
-#df = pd.DataFrame(list(itertools.chain(*[details(item)for item in product_list])))
-df.to_csv("product_detail.csv", index = False)
+    except AttributeError:
+        return
 
-driver.quit()
+    try:
+        rating = product.i.text
+        review_count = product.find('span',{'class':'a-size-base'}).text
+    
+    except:
+        rating = ''
+        review_count = ''
+
+    result=(description,price,rating,review_count,new_url)
+
+    return result
+
+
+def main():
+    from webdriver_manager.chrome import ChromeDriverManager
+
+    searching_item = input("What you want to Search: ")
+
+    driver = webdriver.Chrome(ChromeDriverManager().install())
+
+    record = []
+
+    url = get_url(searching_item)
+
+    for page in range(1,6):
+        driver.get(url.format(page))
+        soup=BeautifulSoup(driver.page_source,'html.parser')
+        results=soup.find_all('div',{'data-component-type':'s-search-result'})
+
+        for product in results:
+            item = detail(product)
+            if item:
+                record.append(item)
+
+    df = pd.DataFrame(record, columns=['Descrpition','Price','Rating','ReviewCount','URL'])
+    df.to_csv('ProductList.csv', index=False)
+    
+    driver.close()
+
+
+main()
